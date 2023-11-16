07e193d16888cc4345f1046b99fc34b5dbe8ea02
yhf <1960437320@qq.com>
2023-11-16 10:15:28 +0800

    lab4

--- lab4/kern/process/proc.c
+++ lab4/kern/process/proc.c
@@ -86,27 +86,22 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
-    //LAB4:EXERCISE1 YOUR CODE
-    /*
-     * below fields in proc_struct need to be initialized
-     *       enum proc_state state;                      // Process state
-     *       int pid;                                    // Process ID
-     *       int runs;                                   // the running times of Proces
-     *       uintptr_t kstack;                           // Process kernel stack
-     *       volatile bool need_resched;                 // bool value: need to be rescheduled to release CPU?
-     *       struct proc_struct *parent;                 // the parent process
-     *       struct mm_struct *mm;                       // Process's memory management field
-     *       struct context context;                     // Switch here to run process
-     *       struct trapframe *tf;                       // Trap frame for current interrupt
-     *       uintptr_t cr3;                              // CR3 register: the base addr of Page Directroy Table(PDT)
-     *       uint32_t flags;                             // Process flag
-     *       char name[PROC_NAME_LEN + 1];               // Process name
-     */
-
-
+        proc->state = PROC_UNINIT;//给进程设置为未初始化状态
+        proc->pid = -1;//未初始化的进程，其pid为-1
+        proc->runs = 0;//初始化时间片,刚刚初始化的进程，运行时间一定为零	
+        proc->kstack = 0;//内核栈地址,该进程分配的地址为0，因为还没有执行，也没有被重定位，因为默认地址都是从0开始的。
+        proc->need_resched = 0;//不需要调度
+        proc->parent = NULL;//父进程为空
+        proc->mm = NULL;//虚拟内存为空
+        memset(&(proc->context), 0, sizeof(struct context));//初始化上下文
+        proc->tf = NULL;//中断帧指针为空
+        proc->cr3 = boot_cr3;//页目录为内核页目录表的基址
+        proc->flags = 0; //标志位为0
+        memset(proc->name, 0, PROC_NAME_LEN);//进程名为0
     }
     return proc;
 }
+
 
 // set_proc_name - set the name of proc
 char *
@@ -160,19 +155,29 @@ get_pid(void) {
 
 // proc_run - make process "proc" running on cpu
 // NOTE: before call switch_to, should load  base addr of "proc"'s new PDT
-void
+// void
 proc_run(struct proc_struct *proc) {
     if (proc != current) {
-        // LAB4:EXERCISE3 YOUR CODE
-        /*
-        * Some Useful MACROs, Functions and DEFINEs, you can use them in below implementation.
-        * MACROs or Functions:
-        *   local_intr_save():        Disable interrupts
-        *   local_intr_restore():     Enable Interrupts
-        *   lcr3():                   Modify the value of CR3 register
-        *   switch_to():              Context switching between two processes
-        */
-       
+        // 只有当proc不是当前执行的线程时，才需要执行
+        bool intr_flag;
+        struct proc_struct *prev = current, *next = proc;
+
+        // 切换时新线程任务时需要暂时关闭中断，避免出现嵌套中断
+        local_intr_save(intr_flag);
+        {
+            current = proc;
+            // 设置TSS任务状态段的esp0的值，令其指向新线程的栈顶
+            // ucore参考Linux的实现，不使用80386提供的TSS任务状态段这一硬件机制实现任务上下文切换，ucore在启动时初始化TSS后(init_gdt)，便不再对其进行修改。
+            // 但进行中断等操作时，依然会用到当前TSS内的esp0属性。发生用户态到内核态中断切换时，硬件会将中断栈帧压入TSS.esp0指向的内核栈中
+            // 因此ucore中的每个线程，需要有自己的内核栈，在进行线程调度切换时，也需要及时的修改esp0的值，使之指向新线程的内核栈顶。
+            load_esp0(next->kstack + KSTACKSIZE);
+            // 设置cr3寄存器的值，令其指向新线程的页表
+            lcr3(next->cr3);
+            // switch_to用于完整的进程上下文切换，定义在统一目录下的switch.S中
+            // 由于涉及到大量的寄存器的存取操作，因此使用汇编实现
+            switch_to(&(prev->context), &(next->context));
+        }
+        local_intr_restore(intr_flag);
     }
 }
 
@@ -299,8 +304,42 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    6. call wakeup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
 
-    
+    // 分配一个未初始化的线程控制块
+    if ((proc = alloc_proc()) == NULL) {
+        goto fork_out;
+    }
+    // 其父进程属于current当前进程
+    proc->parent = current;
 
+    // 设置，分配新线程的内核栈
+    if (setup_kstack(proc) != 0) {
+        // 分配失败，回滚释放之前所分配的内存
+        goto bad_fork_cleanup_proc;
+    }
+    // 由于是fork，因此fork的一瞬间父子线程的内存空间是一致的（clone_flags决定是否采用写时复制）
+    if (copy_mm(clone_flags, proc) != 0) {
+        // 分配失败，回滚释放之前所分配的内存
+        goto bad_fork_cleanup_kstack;
+    }
+    // 复制proc线程时，设置proc的上下文信息
+    copy_thread(proc, stack, tf);
+
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+        // 生成并设置新的pid
+        proc->pid = get_pid();
+        // 加入全局线程控制块哈希表
+        hash_proc(proc);
+        // 加入全局线程控制块双向链表
+        list_add(&proc_list, &(proc->list_link));
+        nr_process ++;
+    }
+    local_intr_restore(intr_flag);
+    // 唤醒proc，令其处于就绪态PROC_RUNNABLE
+    wakeup_proc(proc);
+
+    ret = proc->pid;
 fork_out:
     return ret;
 
